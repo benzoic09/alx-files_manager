65813fe54792ed7648149b5021e44bb88807ca22
steve9948 <mbuguagichimu@gmail.com>
2024-07-03 13:01:22 -0700

    Added the right routes

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,3 @@
-/* eslint-disable import/no-unresolved */
-/* eslint-disable import/extensions */
 import express from 'express';
 import AppController from '../controllers/AppController.js';
 import UsersController from '../controllers/UsersController.js';
@@ -9,13 +7,16 @@ import FilesController from '../controllers/FilesController.js';
 const router = express.Router();
 
 router.get('/status', AppController.getStatus);
-router.get('/stats', AppController.getStatus);
+router.get('/stats', AppController.getStats);
 router.post('/users', UsersController.postNew);
-
 router.get('/connect', AuthController.getConnect);
 router.get('/disconnect', AuthController.getDisconnect);
 router.get('/users/me', UsersController.getMe);
-
 router.post('/files', FilesController.postUpload);
+router.get('/files/:id', FilesController.getShow);
+router.get('/files', FilesController.getIndex);
+router.put('/files/:id/publish', FilesController.putPublish);
+router.put('/files/:id/unpublish', FilesController.putUnpublish);
+router.get('/files/:id/data', FilesController.getFile);
 
 export default router;
